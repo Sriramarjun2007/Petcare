170864d2dd3f48a1c0d0f61aa900c334bd67aa12
Sriram Arjun <sriramarjun2007@gmail.com>
2026-06-18 12:16:40 +0530

    petcare

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,8 @@
 import dogcatlogo from "../assets/dogcat.png";
+import leaf from "../assets/leaf.png";
+import plan from "../assets/plan.png";
+import delivery from "../assets/delivery.png";
+import approve from "../assets/approved.png";
 function Dashboard() {
   return (
     <div className="bg-[#F4EFDE] min-h-screen pt-30">
@@ -30,8 +34,34 @@ function Dashboard() {
         </div>
         <img src={dogcatlogo} alt="petpicture" className="mt-25 "></img>
       </div>
-      <div className="bg-gray-400 mt-20">
-        <h1>Pet Food Subcription</h1>
+      <div className="bg-gray-100 mt-20 h-150">
+        <p className="text-center pt-10 text-3xl font-bold">Pet Food Subcription</p>
+        <p className="text-center text-gray-400 text-xl pt-2">Tailored nutrients plans for your pet's specific needs</p>
+        <div className="flex gap-4 pt-20 pl-40">
+          <div className="w-70 h-70 bg-white rounded-2xl">
+            <img src={leaf} className="p-4"/>
+            <p className="pl-8 text-xl font-bold">Fresh Ingredients</p>
+            <p className="pl-8 pt-5 text-gray-400 ">Real meat,vegetables, and superfoods cooked gently to preserve nutrients</p>
+          </div>
+
+          <div className="w-70 h-70 bg-white rounded-2xl">
+            <img src={plan}  className="p-4"/>
+            <p className="pl-8 text-xl font-bold">Custom Diet<br></br>Plans</p>
+            <p className="pl-8 pt-3 text-gray-400 ">personalized  meals based <br></br>on age ,breed,weight<br></br> and allergies.</p>
+          </div>
+
+          <div className="w-70 h-70 bg-white rounded-2xl">
+            <img src={delivery}  className="p-4"/>
+            <p className="pl-8 text-xl font-bold">Monthly Delivery </p>
+            <p className="pl-8 pt-5 text-gray-400 mr-2">Convenient auto-shipping straight to your door. Never<br></br> run out of food.</p>
+          </div>
+
+          <div className="w-70 h-70 bg-white rounded-2xl">
+            <img src={approve}  className="p-4"/>
+            <p className="pl-8 text-xl font-bold">Vet Approved</p>
+            <p className="pl-8 pt-5 text-gray-400 mr-5">Recipes formulated by veterinary nutritionlists for optimal health.</p>
+          </div>
+        </div>
       </div>
     </div>
   
